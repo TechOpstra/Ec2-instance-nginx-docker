7add893b8ad8660926422fe72f3dc0f5dc1774be
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-19 18:16:47 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,9 +1,13 @@
 module "vpc" {
   source = "./modules/vpc"
+  vpc_cidr = var.vpc_cidr
 }
 
 module "ec2" {
   source = "./modules/ec2"
+  ami = var.ami
+  instance_type = var.instance_type
+  availability_zone = var.availability_zone
   subnet_id = module.vpc.public_subnets[0]
   security_group_id = module.vpc.security_group_id
 }
