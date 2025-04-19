0557b5ce926fb67429fa268298faf57b4d95a553
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-19 12:30:12 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -4,7 +4,6 @@ module "vpc" {
 
 module "ec2" {
   source = "./modules/ec2"
-  vpc_id = module.vpc.vpc_id
   subnet_id = module.vpc.public_subnets[0]
   security_group_id = module.vpc.security_group_id
 }
